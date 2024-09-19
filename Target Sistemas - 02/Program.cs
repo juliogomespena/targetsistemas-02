95959d0e05bcaa3ca426d0b92d98f6793a896ec8
Julio Gomes Pena <juliogomespena@gmail.com>
2024-09-19 18:56:30 -0300

    Create targetsistemas-ex02

---
+++ Target Sistemas - 02/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Informe um número: ");
+
+if (int.TryParse(Console.ReadLine(), out int numero))
+{
+    if (ChecarFibonacci(numero))
+    {
+        Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+    }
+    else
+    {
+        Console.WriteLine($"{numero} não pertence à sequência de Fibonacci.");
+    }
+}
+else
+{
+    Console.WriteLine("Por favor, informe um número válido.");
+}
+
+bool ChecarFibonacci(int numero)
+{
+    if (numero < 0) return false;
+    
+    int a = 0;
+    int b = 1;
+    
+    while (a < numero)
+    {
+        int temp = a;
+        a = b;
+        b = temp + b;
+    }
+
+    return a == numero;
+}
